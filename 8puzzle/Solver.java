a9ad260db95e4760857bbd2331b51f63b8b8970c
Rorschach Lin <294528248@qq.com>
2019-09-20 00:57:48 +0800

    first draft

--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -2,7 +2,11 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+
 public class Solver {
+    private MinPQ<CompareBoard> minPQ;
+    private CompareBoard resultCB;
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
@@ -10,37 +14,75 @@ public class Solver {
             throw new IllegalArgumentException();
         }
 
-        MinPQ<CompareBoard> minPQ = new MinPQ<>();
-        minPQ.insert(new CompareBoard(initial));
+        minPQ = new MinPQ<>();
+        int moveCount = 0;
+        minPQ.insert(new CompareBoard(initial, moveCount, null));
+        while (!minPQ.isEmpty()) {
+            CompareBoard currBoard = minPQ.delMin();
+            if (currBoard.getBoard().isGoal()) {
+                resultCB = currBoard;
+                break;
+            }
+            Iterable<Board> neighbors = currBoard.getBoard().neighbors();
+            for (Board b : neighbors) {
+                minPQ.insert(new CompareBoard(b, currBoard.getMoves() + 1, currBoard));
+            }
+        }
     }
 
     private class CompareBoard implements Comparable<CompareBoard> {
         private final Board board;
+        private final int moves;
+        private final CompareBoard prev;
 
-        CompareBoard(Board board) {
+        CompareBoard(Board board, int moves, CompareBoard prev) {
             this.board = board;
+            this.moves = moves;
+            this.prev = prev;
         }
 
         @Override
         public int compareTo(CompareBoard that) {
             return this.board.manhattan() - that.board.manhattan();
         }
+
+        public Board getBoard() {
+            return board;
+        }
+
+        public int getMoves() {
+            return moves;
+        }
+
+        public int getPriority() {
+            return board.manhattan() + moves;
+        }
+
+        public CompareBoard getPrev() {
+            return prev;
+        }
     }
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
-        return false;
+        return resultCB != null;
     }
 
     // min number of moves to solve initial board
     public int moves() {
-        return 0;
+        return resultCB.getMoves();
     }
 
 
     // sequence of boards in a shortest solution
     public Iterable<Board> solution() {
-        return null;
+        ArrayList<Board> result = new ArrayList<>();
+        CompareBoard currBoard = resultCB;
+        while (currBoard.getPrev() != null) {
+            result.add(currBoard.getBoard());
+            currBoard = currBoard.getPrev();
+        }
+        return result;
     }
 
     // test client (see below)
